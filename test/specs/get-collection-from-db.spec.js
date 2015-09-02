c7a2a57d6282e06ddd818164a856dc0170b60fd1
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-09-02 15:30:21 +0100

    refactored last test

--- test/specs/get-collection-from-db.spec.js
+++ test/specs/get-collection-from-db.spec.js
@@ -1,4 +1,4 @@
-var test              = require('tape');
+var assert            = require('assert');
 var sinon             = require('sinon');
 var getDBName         = require('../helpers/get-db-name');
 var cleanDB           = require('../helpers/clean-db');
@@ -10,157 +10,171 @@ var getCollectionFrom = require('../../lib/get-collection-from-db');
 var console   = (window.console || {});
 console.error = !!console.error ? console.error.bind(console) : function(){};
 
-test('getCollectionFrom()', function (t){
+describe('getCollectionFrom()', function (done){
 
-  test('Should reject if no DB is passed', function (t){
+  describe('The arguments', function (){
 
-    var spy = sinon.spy();
+    it('Should reject if no DB is passed', function (done){
 
-    getCollectionFrom()
-      .catch(spy)
-      .then(function(){
-        t.assert(spy.callCount, 'getCollectionFrom promise was rejected');
-        t.end();
-      })
-      .catch(console.error);
-  });
+      var spy = sinon.spy();
 
-  test('Should reject if an invalid DB is passed', function (t){
+      getCollectionFrom()
+        .catch(spy)
+        .then(function(){
+          assert(spy.callCount, 'getCollectionFrom promise was rejected');
+          done();
+        })
+        .catch(console.error);
+    });
 
-    var spy = sinon.spy();
+    it('Should reject if an invalid DB is passed', function (done){
 
-    getCollectionFrom(true)
-      .catch(spy)
-      .then(function(){
-        t.assert(spy.callCount, 'getCollectionFrom promise was rejected');
-        t.end();
-      })
-      .catch(console.error);
-  });
+      var spy = sinon.spy();
 
-  test('Should reject if no storeName is passed', function (t){
+      getCollectionFrom(true)
+        .catch(spy)
+        .then(function(){
+          assert(spy.callCount, 'getCollectionFrom promise was rejected');
+          done();
+        })
+        .catch(console.error);
+    });
 
-    var spy = sinon.spy();
-    var dbName = getDBName();
+    it('Should reject if no storeName is passed', function (done){
 
-    createDB({ name: dbName, versionn: 1})
-      .then( function (db){
-         return getCollectionFrom(db);
-      })
-      .catch(spy)
-      .then(function(){
-        t.assert(spy.callCount, 'getCollectionFrom promise was rejected');
-        t.end();
-      })
-      .catch(console.error);
+      var spy = sinon.spy();
+      var dbName = getDBName();
 
-  });
+      createDB({ name: dbName, versionn: 1})
+        .then( function (db){
+           return getCollectionFrom(db);
+        })
+        .catch(spy)
+        .then(function(){
+          assert(spy.callCount, 'getCollectionFrom promise was rejected');
+          done();
+        })
+        .catch(console.error);
 
-  test('Should reject if no limit is passed', function (t){
+    });
 
-    var spy = sinon.spy();
-    var dbName = getDBName();
+    it('Should reject if no limit is passed', function (done){
 
-    createDB({ name: dbName, version: 1})
-      .then( function (db){
-         return getCollectionFrom(db, 'obj1');
-      })
-      .catch(spy)
-      .then(function(){
-        t.assert(spy.callCount, 'getCollectionFrom promise was rejected');
-        t.end();
-      })
-      .catch(console.error);
+      var spy = sinon.spy();
+      var dbName = getDBName();
 
-  });
+      createDB({ name: dbName, version: 1})
+        .then( function (db){
+           return getCollectionFrom(db, 'obj1');
+        })
+        .catch(spy)
+        .then(function(){
+          assert(spy.callCount, 'getCollectionFrom promise was rejected');
+          done();
+        })
+        .catch(console.error);
 
-  test('Should retrieve all members of a given collection after the limit', function (t){
+    });
 
-    var dbName = getDBName();
-
-    createDB({ name: dbName, version: 1, objects: [{ name: 'obj1', indexes: [{ name: 'prop' }] }] })
-      .then( function (db){
-        t.ok(db,'Returned db okay');
-        return [db, addToDB(db, 'obj1', [
-          { prop: 'test1' },
-          { prop: 'test2' },
-          { prop: 'test3' },
-          { prop: 'test4' }
-        ])];
-      })
-      .spread(function (db){
-        console.log('added');
-        return [db, getCollectionFrom(db, 'obj1', 3)];
-      })
-      .spread( function (db, collection){
-        t.ok(collection, 'got collection back okay');
-        t.equal(collection.length, 2, 'got all of the collection');
-        t.equal(collection[0].prop , 'test3', 'got first collection item okay');
-        t.equal(collection[1].prop , 'test4', 'got second collection item okay');
-        cleanDB(db);
-        t.end();
-      })
-      .catch(console.error);
-  });
-
-  test('Should retrieve all members of a given collection before the limit if a reverse direction is passed', function (t){
-
-    var dbName = getDBName();
-
-    createDB({ name: dbName, version: 1, objects: [{ name: 'obj1', indexes: [{ name: 'prop' }] }] })
-      .then( function (db){
-        t.ok(db,'Returned db okay');
-        return [db, addToDB(db, 'obj1', [
-          { prop: 'test1' },
-          { prop: 'test2' },
-          { prop: 'test3' },
-          { prop: 'test4' }
-        ])];
-      })
-      .spread(function (db){
-        console.log('added');
-        return [db, getCollectionFrom(db, 'obj1', 2, null, 'prev')];
-      })
-      .spread( function (db, collection){
-        t.ok(collection, 'got collection back okay');
-        t.equal(collection.length, 2, 'got all of the collection');
-        t.equal(collection[0].prop , 'test2', 'got first collection item okay');
-        t.equal(collection[1].prop , 'test1', 'got second collection item okay');
-        cleanDB(db);
-        t.end();
-      })
-      .catch(console.error);
   });
 
 
-  test('Should retrieve all members of a given collection before the limit if a bound type is passed', function (t){
+  describe('Get after', function (){
 
-    var dbName = getDBName();
+    it('Should retrieve all members of a given collection after the limit', function (done){
 
-    createDB({ name: dbName, version: 1, objects: [{ name: 'obj1', indexes: [{ name: 'prop' }] }] })
-      .then( function (db){
-        t.ok(db,'Returned db okay');
-        return [db, addToDB(db, 'obj1', [
-          { prop: 'test1' },
-          { prop: 'test2' },
-          { prop: 'test3' },
-          { prop: 'test4' }
-        ])];
-      })
-      .spread(function (db){
-        console.log('added');
-        return [db, getCollectionFrom(db, 'obj1', 3, null, 'prev', 'lowerBound')];
-      })
-      .spread( function (db, collection){
-        t.ok(collection, 'got collection back okay');
-        t.equal(collection.length, 2, 'got all of the collection');
-        t.equal(collection[0].prop , 'test4', 'got first collection item okay');
-        t.equal(collection[1].prop , 'test3', 'got second collection item okay');
-        cleanDB(db);
-        t.end();
-      })
-      .catch(console.error);
+      var dbName = getDBName();
+
+      createDB({ name: dbName, version: 1, objects: [{ name: 'obj1', indexes: [{ name: 'prop' }] }] })
+        .then( function (db){
+          assert(db,'Returned db okay');
+          return [db, addToDB(db, 'obj1', [
+            { prop: 'it1' },
+            { prop: 'it2' },
+            { prop: 'it3' },
+            { prop: 'it4' }
+          ])];
+        })
+        .spread(function (db){
+          return [db, getCollectionFrom(db, 'obj1', 3)];
+        })
+        .spread( function (db, collection){
+          assert(collection, 'got collection back okay');
+          assert.equal(collection.length, 2, 'got all of the collection');
+          assert.equal(collection[0].prop , 'it3', 'got first collection item okay');
+          assert.equal(collection[1].prop , 'it4', 'got second collection item okay');
+          cleanDB(db);
+          done();
+        })
+        .catch(console.error);
+    });
+
   });
 
-  t.end();
+  describe('Get before', function (){
+
+    it('Should retrieve all members of a given collection before the limit if a reverse direction is passed', function (done){
+
+      var dbName = getDBName();
+
+      createDB({ name: dbName, version: 1, objects: [{ name: 'obj1', indexes: [{ name: 'prop' }] }] })
+        .then( function (db){
+          assert(db,'Returned db okay');
+          return [db, addToDB(db, 'obj1', [
+            { prop: 'it1' },
+            { prop: 'it2' },
+            { prop: 'it3' },
+            { prop: 'it4' }
+          ])];
+        })
+        .spread(function (db){
+          console.log('added');
+          return [db, getCollectionFrom(db, 'obj1', 2, null, 'prev')];
+        })
+        .spread( function (db, collection){
+          assert(collection, 'got collection back okay');
+          assert.equal(collection.length, 2, 'got all of the collection');
+          assert.equal(collection[0].prop , 'it2', 'got first collection item okay');
+          assert.equal(collection[1].prop , 'it1', 'got second collection item okay');
+          cleanDB(db);
+          done();
+        })
+        .catch(console.error);
+    });
+
+  });
+
+  describe('Using bounds', function (){
+
+    it('Should retrieve all members of a given collection before the limit if a bound type is passed', function (done){
+
+      var dbName = getDBName();
+
+      createDB({ name: dbName, version: 1, objects: [{ name: 'obj1', indexes: [{ name: 'prop' }] }] })
+        .then( function (db){
+          assert(db,'Returned db okay');
+          return [db, addToDB(db, 'obj1', [
+            { prop: 'it1' },
+            { prop: 'it2' },
+            { prop: 'it3' },
+            { prop: 'it4' }
+          ])];
+        })
+        .spread(function (db){
+          console.log('added');
+          return [db, getCollectionFrom(db, 'obj1', 3, null, 'prev', 'lowerBound')];
+        })
+        .spread( function (db, collection){
+          assert(collection, 'got collection back okay');
+          assert.equal(collection.length, 2, 'got all of the collection');
+          assert.equal(collection[0].prop , 'it4', 'got first collection item okay');
+          assert.equal(collection[1].prop , 'it3', 'got second collection item okay');
+          cleanDB(db);
+          done();
+        })
+        .catch(console.error);
+    });
+
+  });
+
 });
