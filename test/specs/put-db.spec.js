ba168b3a9dd6d1601d79f2f44c3e56faa3afd7ec
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-09-02 15:12:50 +0100

    refactorer add-db.spec.js

--- test/specs/put-db.spec.js
+++ test/specs/put-db.spec.js
@@ -1,3 +1,4 @@
+var assert    = require('assert');
 var test      = require('tape');
 var sinon     = require('sinon');
 var getDBName = require('../helpers/get-db-name');
@@ -9,157 +10,166 @@ var addToDB   = require('../../lib/put-db');
 var console = (window.console || {});
 console.error = !!console.error ? console.error.bind(console) : function(){};
 
-test('addToDB', function (t){
+describe('addToDB', function (){
 
-  test('Should reject if no DB is passed', function (t){
+  describe('The arguments', function (){
 
-    var spy = sinon.spy();
+    it('Should reject if no DB is passed', function (done){
 
-    addToDB()
-      .catch(spy)
-      .then(function(){
-        t.assert(spy.callCount, 'addToDB promise was rejected');
-        t.end();
-      })
-      .catch(console.error);
-  });
+      var spy = sinon.spy();
 
-  test('Should reject if an invalid db is passed', function (t){
+      addToDB()
+        .catch(spy)
+        .then(function(){
+          assert(spy.callCount, 'addToDB promise was rejected');
+          done();
+        })
+        .catch(console.error);
+    });
 
-    var spy = sinon.spy();
+    it('Should reject if an invalid db is passed', function (done){
 
-    addToDB({db: true})
-      .catch(spy)
-      .then(function (){
-        t.assert(spy.callCount, 'addToDB promise was rejected');
-        t.end();
-      })
-      .catch(console.error);
-  });
+      var spy = sinon.spy();
 
-  test('Should reject if no object store is passed', function (t){
+      addToDB({db: true})
+        .catch(spy)
+        .then(function (){
+          assert(spy.callCount, 'addToDB promise was rejected');
+          done();
+        })
+        .catch(console.error);
+    });
 
-    var dataBase;
-    var spy    = sinon.spy();
-    var dbName = getDBName();
+    it('Should reject if no object store is passed', function (done){
 
-    createDB({ name: dbName, version: 1 })
-      .then(function(db){
-        t.ok(db, 'db created successfully');
-        dataBase = db;
-        return addToDB(db);
-      })
-      .catch(spy)
-      .then(function(){
-        t.assert(spy.callCount, 'addToDB promise was rejected');
-        cleanDB(dataBase);
-        t.end();
-      })
-      .catch(console.error);
-  });
+      var dataBase;
+      var spy    = sinon.spy();
+      var dbName = getDBName();
 
-  test('Should reject if an invalid object store name is passed', function (t){
-
-    var dataBase;
-    var dbName = getDBName();
-    var spy    = sinon.spy();
-
-    createDB({ name: dbName, version: 1, objects: [{ name: 'obj1' }] })
-      .then(function(db){
-        dataBase = db;
-        return addToDB(db, 'obj2');
-      })
-      .catch(spy)
-      .then(function(){
-        t.assert(spy.callCount, 'addToDB promise was rejected');
-        cleanDB(dataBase);
-        t.end();
-      })
-      .catch(console.error);
-  });
-
-  test('Should reject if no object is passed', function (t){
-
-    var dataBase;
-    var dbName = getDBName();
-    var spy    = sinon.spy();
-
-    createDB({ name: dbName, version: 1, objects: [{ name: 'obj1' }] })
-      .then(function(db){
-        dataBase = db;
-        return addToDB(db, 'obj1');
-      })
-      .catch(spy)
-      .then(function(){
-        t.assert(spy.callCount, 'addToDB promise was rejected');
-        cleanDB(dataBase);
-        t.end();
-      })
-      .catch(console.error);
-  });
-
-  test('Should add an object to the database', function (t){
-
-    var dataBase;
-    var dbName = getDBName();
-
-    createDB({
-      name: dbName, version: 1, objects: [{
-        name: 'obj1'
-      }]
-    })
-    .then(function(db){
-      dataBase = db;
-      return [db, addToDB(db, 'obj1', { prop: 'test' })];
-    })
-    .spread(function(db, index){
-      db.transaction(['obj1'])
-        .objectStore('obj1')
-        .get(index)
-        .onsuccess = function(e){
-          t.equal(e.target.result.prop, 'test');
+      createDB({ name: dbName, version: 1 })
+        .then(function(db){
+          assert(db, 'db created successfully');
+          dataBase = db;
+          return addToDB(db);
+        })
+        .catch(spy)
+        .then(function(){
+          assert(spy.callCount, 'addToDB promise was rejected');
           cleanDB(dataBase);
-          t.end();
-        };
-    })
-    .catch(console.error);
+          done();
+        })
+        .catch(console.error);
+    });
+
+    it('Should reject if an invalid object store name is passed', function (done){
+
+      var dataBase;
+      var dbName = getDBName();
+      var spy    = sinon.spy();
+
+      createDB({ name: dbName, version: 1, objects: [{ name: 'obj1' }] })
+        .then(function(db){
+          dataBase = db;
+          return addToDB(db, 'obj2');
+        })
+        .catch(spy)
+        .then(function(){
+          assert(spy.callCount, 'addToDB promise was rejected');
+          cleanDB(dataBase);
+          done();
+        })
+        .catch(console.error);
+    });
+
+    it('Should reject if no object is passed', function (done){
+
+      var dataBase;
+      var dbName = getDBName();
+      var spy    = sinon.spy();
+
+      createDB({ name: dbName, version: 1, objects: [{ name: 'obj1' }] })
+        .then(function(db){
+          dataBase = db;
+          return addToDB(db, 'obj1');
+        })
+        .catch(spy)
+        .then(function(){
+          assert(spy.callCount, 'addToDB promise was rejected');
+          cleanDB(dataBase);
+          done();
+        })
+        .catch(console.error);
+    });
+
   });
 
-  test('Should add a collection the database', function (t){
+  describe('Adding objects', function (){
 
-    var dataBase;
-    var dbName = getDBName();
+    it('Should add an object to the database', function (done){
 
-    createDB({
-      name: dbName, version: 1, objects: [{
-        name: 'obj1'
-      }]
-    })
-    .then(function(db){
-      dataBase = db;
-      return [db, addToDB(db, 'obj1', [{ id: 1, prop: 'test' }, { id: 2, prop: 'test2'}])];
-    })
-    //Getting things out of the db like ths is gross
-    .spread(function(db){
-      //get the first object out of the database
-      db.transaction(['obj1'])
-        .objectStore('obj1')
-        .get(1)
-        .onsuccess = function(e){
-          t.equal(e.target.result.prop, 'test', 'The first object pulled from the DB is as expected');
-          //get the second object out of the database
-          db.transaction(['obj1'])
-            .objectStore('obj1')
-            .get(2)
-            .onsuccess = function(e){
-              t.equal(e.target.result.prop, 'test2');
-              t.end();
-              cleanDB(db);
-            };
-        };
-    })
-    .catch(console.error);
+      var dataBase;
+      var dbName = getDBName();
+
+      createDB({
+        name: dbName, version: 1, objects: [{
+          name: 'obj1'
+        }]
+      })
+      .then(function(db){
+        dataBase = db;
+        return [db, addToDB(db, 'obj1', { prop: 'test' })];
+      })
+      .spread(function(db, index){
+        db.transaction(['obj1'])
+          .objectStore('obj1')
+          .get(index)
+          .onsuccess = function(e){
+            assert.equal(e.target.result.prop, 'test');
+            cleanDB(dataBase);
+            done();
+          };
+      })
+      .catch(console.error);
+    });
+
   });
 
-  t.end();
+  describe('Adding collections', function (){
+    it('Should add a collection the database', function (done){
+
+      var dataBase;
+      var dbName = getDBName();
+
+      createDB({
+        name: dbName, version: 1, objects: [{
+          name: 'obj1'
+        }]
+      })
+      .then(function(db){
+        dataBase = db;
+        return [db, addToDB(db, 'obj1', [{ id: 1, prop: 'test' }, { id: 2, prop: 'test2'}])];
+      })
+      //Getting things out of the db like ths is gross
+      .spread(function(db){
+        //get the first object out of the database
+        db.transaction(['obj1'])
+          .objectStore('obj1')
+          .get(1)
+          .onsuccess = function(e){
+            assert.equal(e.target.result.prop, 'test', 'The first object pulled from the DB is as expected');
+            //get the second object out of the database
+            db.transaction(['obj1'])
+              .objectStore('obj1')
+              .get(2)
+              .onsuccess = function(e){
+                assert.equal(e.target.result.prop, 'test2');
+                done();
+                cleanDB(db);
+              };
+          };
+      })
+      .catch(console.error);
+    });
+  });
+
 });
