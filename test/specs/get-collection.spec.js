49921456475436fb036fc01f6a4a11f2b393b54a
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-09-02 15:24:45 +0100

    refactored get collection

--- test/specs/get-collection.spec.js
+++ test/specs/get-collection.spec.js
@@ -1,4 +1,4 @@
-var test          = require('tape');
+var assert        = require('assert');
 var sinon         = require('sinon');
 var getDBName     = require('../helpers/get-db-name');
 var cleanDB       = require('../helpers/clean-db');
@@ -10,112 +10,120 @@ var getCollection = require('../../lib/get-collection-db');
 var console   = (window.console || {});
 console.error = !!console.error ? console.error.bind(console) : function(){};
 
-test('getCollection()', function (t){
+describe('getCollection()', function (done){
 
-  test('Should reject if no DB is passed', function (t){
+  describe('The arguments', function (){
 
-    var spy = sinon.spy();
+    it('Should reject if no DB is passed', function (done){
 
-    getCollection()
+      var spy = sinon.spy();
+
+      getCollection()
+        .catch(spy)
+        .then(function(){
+          assert(spy.callCount, 'getCollection promise was rejected');
+          done();
+        })
+        .catch(console.error);
+    });
+
+    it('Should reject if an invalid DB is passed', function (done){
+
+      var spy = sinon.spy();
+
+      getCollection(true)
+        .catch(spy)
+        .then(function(){
+          assert(spy.callCount, 'getCollection promise was rejected');
+          done();
+        })
+        .catch(console.error);
+    });
+
+    it('Should reject if no storeName is passed', function (done){
+
+      var dataBase;
+      var spy = sinon.spy();
+      var dbName = getDBName();
+
+      createDB({ name: dbName, version: 1 })
+      .then( function (db){
+        assert(db, 'Got a database back');
+        dataBase = db;
+        return getCollection(db);
+      })
       .catch(spy)
       .then(function(){
-        t.assert(spy.callCount, 'getCollection promise was rejected');
-        t.end();
+        assert(spy.callCount, 'getCollection promise was rejected');
+        cleanDB(dataBase);
+        done();
       })
       .catch(console.error);
+    });
+
   });
 
-  test('Should reject if an invalid DB is passed', function (t){
+  describe('Getting a collection', function (){
 
-    var spy = sinon.spy();
+    it('Should retrieve all members of a given collection', function (done){
 
-    getCollection(true)
-      .catch(spy)
-      .then(function(){
-        t.assert(spy.callCount, 'getCollection promise was rejected');
-        t.end();
-      })
-      .catch(console.error);
+      var dbName = getDBName();
+
+      createDB({ name: dbName, version: 1, objects: [{ name: 'obj1', indexes: [{ name: 'prop' }] }] })
+        .then( function (db){
+          assert(db,'Returned db okay');
+          return [db, addToDB(db, 'obj1', [
+            { prop: 'it1' },
+            { prop: 'it2' },
+            { prop: 'it3' },
+            { prop: 'it4' }
+          ])];
+        })
+        .spread(function (db){
+          return [db, getCollection(db, 'obj1')];
+        })
+        .spread( function (db, collection){
+          assert(collection, 'got collection back okay');
+          assert.equal(collection.length, 4, 'got all of the collection');
+          assert.equal(collection[0].prop , 'it1', 'got first collection item okay');
+          assert.equal(collection[1].prop , 'it2', 'got second collection item okay');
+          assert.equal(collection[2].prop , 'it3', 'got third collection item okay');
+          assert.equal(collection[3].prop , 'it4', 'got fourth collection item okay');
+          cleanDB(db);
+          done();
+        })
+        .catch(console.error);
+    });
+
   });
 
-  test('Should reject if no storeName is passed', function (t){
+  describe('Getting a collection on an index', function (){
+    it('Should get collections by index', function (done){
 
-    var dataBase;
-    var spy = sinon.spy();
-    var dbName = getDBName();
+      var dbName = getDBName();
 
-    createDB({ name: dbName, version: 1 })
-    .then( function (db){
-      t.ok(db, 'Got a database back');
-      dataBase = db;
-      return getCollection(db);
-    })
-    .catch(spy)
-    .then(function(){
-      t.assert(spy.callCount, 'getCollection promise was rejected');
-      cleanDB(dataBase);
-      t.end();
-    })
-    .catch(console.error);
+      createDB({ name: dbName, version: 1, objects: [{ name: 'obj1', indexes: [{ name: 'prop' }] }] })
+        .then( function (db){
+          assert(db,'Returned db okay');
+          return [db, addToDB(db, 'obj1', [
+            { prop: 'it1' },
+            { prop: 'it1' },
+            { prop2: 'it2' },
+            { prop2: 'it2' }
+          ])];
+        })
+        .spread(function (db){
+          return [db, getCollection(db, 'obj1', 'prop')];
+        })
+        .spread( function (db, collection){
+          assert(collection, 'got collection back okay');
+          assert.equal(collection.length, 2, 'got all of the collection');
+          assert.equal(collection[0].prop , 'it1', 'got first collection item okay');
+          assert.equal(collection[1].prop , 'it1', 'got first collection item okay');
+          done();
+        })
+        .catch(console.error);
+    });
   });
 
-  test('Should retrieve all members of a given collection', function (t){
-
-    var dbName = getDBName();
-
-    createDB({ name: dbName, version: 1, objects: [{ name: 'obj1', indexes: [{ name: 'prop' }] }] })
-      .then( function (db){
-        t.ok(db,'Returned db okay');
-        return [db, addToDB(db, 'obj1', [
-          { prop: 'test1' },
-          { prop: 'test2' },
-          { prop: 'test3' },
-          { prop: 'test4' }
-        ])];
-      })
-      .spread(function (db){
-        return [db, getCollection(db, 'obj1')];
-      })
-      .spread( function (db, collection){
-        t.ok(collection, 'got collection back okay');
-        t.equal(collection.length, 4, 'got all of the collection');
-        t.equal(collection[0].prop , 'test1', 'got first collection item okay');
-        t.equal(collection[1].prop , 'test2', 'got second collection item okay');
-        t.equal(collection[2].prop , 'test3', 'got third collection item okay');
-        t.equal(collection[3].prop , 'test4', 'got fourth collection item okay');
-        cleanDB(db);
-        t.end();
-      })
-      .catch(console.error);
-  });
-
-
-  test('Should get collections by index', function (t){
-
-    var dbName = getDBName();
-
-    createDB({ name: dbName, version: 1, objects: [{ name: 'obj1', indexes: [{ name: 'prop' }] }] })
-      .then( function (db){
-        t.ok(db,'Returned db okay');
-        return [db, addToDB(db, 'obj1', [
-          { prop: 'test1' },
-          { prop: 'test1' },
-          { prop2: 'test2' },
-          { prop2: 'test2' }
-        ])];
-      })
-      .spread(function (db){
-        return [db, getCollection(db, 'obj1', 'prop')];
-      })
-      .spread( function (db, collection){
-        t.ok(collection, 'got collection back okay');
-        t.equal(collection.length, 2, 'got all of the collection');
-        t.equal(collection[0].prop , 'test1', 'got first collection item okay');
-        t.equal(collection[1].prop , 'test1', 'got first collection item okay');
-        t.end();
-      })
-      .catch(console.error);
-  });
-
-  t.end();
 });
