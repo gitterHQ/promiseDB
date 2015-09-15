371c86ae11d4fa5c5f56fd9b3908252b224ca7d3
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-09-15 14:44:29 +0100

    expose functionality

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 module.exports = {
-  createDB: require('./lib/create-db')
+  createDB:          require('./lib/create-db'),
+  get:               require('./lib/get-db'),
+  put:               require('./lib/put-db'),
+  getCollection:     require('./lib/get-collection-db.js'),
+  getCollectionFrom: require('./lib/get-collection-from-db')
 };
