a7db68b14d357519b44320f1419d497f3e6b5c7e
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-09-23 14:19:28 +0100

    added delete db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   createDB:          require('./lib/create-db'),
+  deleteDB:          require('./lib/delete-db.js'),
   get:               require('./lib/get-db'),
   put:               require('./lib/put-db'),
   getCollection:     require('./lib/get-collection-db.js'),
